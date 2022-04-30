f85d51cecf5eb154e72d4bc548cac7e93f1a108a
Ufuk Enes Kurt <83250239+UEKAPPS@users.noreply.github.com>
2022-04-30 22:56:04 +0300

    Merhaba Dünya
    Merhaba Dünya

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Merhaba Dünya");
+		
+		//Yorum Satýrý	
+		
+	}
+
+}
